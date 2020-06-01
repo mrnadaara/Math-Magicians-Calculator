338a55ca798302127b165c18f10dddc4f020579a
Sharmarke Ahmed <somaliwarsan@gmail.com>
2020-06-01 20:07:05 +0300

    added react-bootstrap and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import App from './App';
 
 ReactDOM.render(
