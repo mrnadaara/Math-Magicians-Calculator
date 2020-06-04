310f4136c909c8dc67b16c3595e6c784a9d5e060
Sharmarke Ahmed <somaliwarsan@gmail.com>
2020-06-04 13:59:14 +0300

    created and implemented components for calculator

---
+++ src/components/ButtonPanel/ButtonPanel.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Button from '../Button/Button';
+
+import './ButtonPanel.scss';
+
+const ButtonPanel = () => (
+  <div className="button-panel">
+    <div className="group">
+      <Button name="AC" />
+      <Button name="+/-" />
+      <Button name="%" />
+      <Button name="÷" />
+    </div>
+    <div className="group">
+      <Button name="7" />
+      <Button name="8" />
+      <Button name="9" />
+      <Button name="X" />
+    </div>
+    <div className="group">
+      <Button name="4" />
+      <Button name="5" />
+      <Button name="6" />
+      <Button name="-" />
+    </div>
+    <div className="group">
+      <Button name="1" />
+      <Button name="2" />
+      <Button name="3" />
+      <Button name="+" />
+    </div>
+    <div className="group">
+      <Button name="0" />
+      <Button name="." />
+      <Button name="=" />
+    </div>
+  </div>
+);
+
+export default ButtonPanel;
