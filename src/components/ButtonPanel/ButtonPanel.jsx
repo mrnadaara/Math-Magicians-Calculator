31e0fda99f4bb45d14810bdd66910d543af7141b
Sharmarke Ahmed <somaliwarsan@gmail.com>
2020-06-08 18:11:30 +0300

    altered stylings to fit requirements

--- src/components/ButtonPanel/ButtonPanel.jsx
+++ src/components/ButtonPanel/ButtonPanel.jsx
@@ -7,32 +7,32 @@ import './ButtonPanel.scss';
 const ButtonPanel = () => (
   <div className="button-panel">
     <div className="group">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" />
+      <Button name="AC" color="lightgrey" />
+      <Button name="+/-" color="lightgrey" />
+      <Button name="%" color="lightgrey" />
+      <Button name="/" />
     </div>
     <div className="group">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
+      <Button name="7" color="lightgrey" />
+      <Button name="8" color="lightgrey" />
+      <Button name="9" color="lightgrey" />
       <Button name="X" />
     </div>
     <div className="group">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
+      <Button name="4" color="lightgrey" />
+      <Button name="5" color="lightgrey" />
+      <Button name="6" color="lightgrey" />
       <Button name="-" />
     </div>
     <div className="group">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
+      <Button name="1" color="lightgrey" />
+      <Button name="2" color="lightgrey" />
+      <Button name="3" color="lightgrey" />
       <Button name="+" />
     </div>
     <div className="group">
-      <Button name="0" wide />
-      <Button name="." />
+      <Button name="0" wide color="lightgrey" />
+      <Button name="." color="lightgrey" />
       <Button name="=" />
     </div>
   </div>
